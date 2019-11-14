58be8fb8f72f95e3dd174d904b56707f463a0179
cydn95 <devstar.sky20@gmail.com>
2019-11-13 19:33:45 -0800

    add aoquestion api

--- mysite/settings.py
+++ mysite/settings.py
@@ -314,7 +314,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
 
 CORS_ORIGIN_ALLOW_ALL = True
