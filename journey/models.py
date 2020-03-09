c84ef3c77e27f74c833aa80be3ee3978460f9240
cydn95 <devstar.sky20@gmail.com>
2020-03-09 01:35:58 -0700

    fix some mistypos

--- journey/models.py
+++ journey/models.py
@@ -1,6 +1,12 @@
 from django.db import models
+from rgbfield.fields import RGBColorField
 
 # Create your models here.
 class Journey(models.Model):
     pageName = models.CharField(max_length=50)
     pageLink = models.URLField(max_length=255)
+    xPos = models.IntegerField()
+    yPos = models.IntegerField()
+    color = RGBColorField(default="#afd8f8")
+    orderId = models.PositiveIntegerField()
+    
