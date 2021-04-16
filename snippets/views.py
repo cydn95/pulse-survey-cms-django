f1f206ffb361797ef5f8b0223566b99c231a15aa
cydn95 <devstar.sky20@gmail.com>
2021-04-16 03:10:29 -0700

    fix issue in advisor api

--- snippets/views.py
+++ snippets/views.py
@@ -2601,7 +2601,7 @@ class AdvisorInsightsView(APIView):
         # pessimistic would be the groups with lowest aws comprehend scores (like top negative)
 
         # "least share to share their opinions" groups with the lowest score for this question
-        
+
         # Respondant: counts of the SHCategory who have got at least 1 AM or AO response
         myPeersShCategoryId = SHCategory.objects.get(survey=survey, SHCategoryName="My Peers").id
         whoINeedShCategoryId = SHCategory.objects.get(survey=survey, SHCategoryName="Who I Need").id
@@ -2636,16 +2636,23 @@ class AdvisorInsightsView(APIView):
         aoresponsereportserializer = AOResponseForDriverAnalysisSerializer(aoresponsereportqueryset, many=True)
         aoresponsereportdata = aoresponsereportserializer.data
 
+        aryTeams = []
         for i in range(len(amresponsereportdata)):
-            amquestionqueryset = AMQuestion.objects.filter(
-                id=amresponsereportdata[i]['amQuestion'])
-            amserializer = AMQuestionSerializer(amquestionqueryset, many=True)
-            amresponsereportdata[i]['amQuestionData'] = amserializer.data
+            if amresponsereportdata[i]['projectUser']["team"]["id"] not in aryTeams:
+                aryTeams.append(
+                    amresponsereportdata[i]['projectUser']["team"]["id"])
+            # amquestionqueryset = AMQuestion.objects.filter(
+            #     id=amresponsereportdata[i]['amQuestion'])
+            # amserializer = AMQuestionSerializer(amquestionqueryset, many=True)
+            # amresponsereportdata[i]['amQuestionData'] = amserializer.data
 
         for j in range(len(aoresponsereportdata)):
-            aoquestionqueryset = AOQuestion.objects.filter(id=aoresponsereportdata[j]['aoQuestion'])
-            aoserializer = AOQuestionSerializer(aoquestionqueryset, many=True)
-            aoresponsereportdata[j]['aoQuestionData'] = aoserializer.data
+            if aoresponsereportdata[j]['projectUser']["team"]["id"] not in aryTeams:
+                aryTeams.append(aoresponsereportdata[j]['projectUser']['team']['id'])
+
+            # aoquestionqueryset = AOQuestion.objects.filter(id=aoresponsereportdata[j]['aoQuestion'])
+            # aoserializer = AOQuestionSerializer(aoquestionqueryset, many=True)
+            # aoresponsereportdata[j]['aoQuestionData'] = aoserializer.data
         
         responseData = amresponsereportdata + aoresponsereportdata
 
@@ -2653,7 +2660,10 @@ class AdvisorInsightsView(APIView):
         recommendedProjectUserSerializer = ProjectUserForReportSerializer(
             recommendedProjectUsersQuerySet, many=True)
         
-        responseRateFromInvitedTeamMembers = 70
+        project = Survey.objects.get(pk=survey).project
+        totalTeamMembersCnt = Team.objects.filter(project=project).count()
+
+        responseRateFromInvitedTeamMembers = len(aryTeams) * 100 / totalTeamMembersCnt
         responseRateFromInvitedStakeholders = 30
         totalDepartments = 5
 
