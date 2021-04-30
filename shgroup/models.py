ef99ce986b958a57a453bc8138b21da438e09bd2
cydn95 <devstar.sky20@gmail.com>
2021-04-29 19:56:28 -0700

    fix typo issue

--- shgroup/models.py
+++ shgroup/models.py
@@ -137,7 +137,7 @@ class ProjectUser(models.Model):
                 email.attach(image_connect)
                 image_connect.add_header('Content-ID', f"<{image_name_connect}>")
 
-            email.send()
+            # email.send()
 
 # class ProjectUserForm(ModelForm):
 #     userPermission = forms.ModelMultipleChoiceField(queryset=Permission.objects.all(), required=False)
