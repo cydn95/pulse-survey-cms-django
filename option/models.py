8f4df21c4741e8725735ffb51303ac330e3a023c
cydn95 <devstar.sky20@gmail.com>
2021-06-03 09:01:11 -0700

    update option model

--- option/models.py
+++ option/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from survey.models import Survey
 
 # Create your models here.
 class Option(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
     optionName = models.CharField(max_length=255)
 
     def __str__(self):
