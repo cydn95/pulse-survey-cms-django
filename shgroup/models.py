43af47477799230eaea70e146d2b278179e27421
cydn95 <devstar.sky20@gmail.com>
2021-06-13 18:47:17 -0700

    add default value in projectorganization

--- shgroup/models.py
+++ shgroup/models.py
@@ -64,7 +64,7 @@ class ProjectUser(models.Model):
     # added superuser toggle
     isSuperUser = models.BooleanField(
         default=False, verbose_name='Reveal Dashboards')
-    projectOrganization = models.CharField(max_length=200, verbose_name='Project Organization', blank=True)
+    projectOrganization = models.CharField(max_length=200, verbose_name='Project Organization', blank=True, default='')
 
     isTeamMember = models.BooleanField(default=False)
     isCGroup1 = models.BooleanField(default=False)
