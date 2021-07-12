89c7faf8d512dd9c28b48680b301ac766400d1f7
cydn95 <devstar.sky20@gmail.com>
2021-07-11 19:16:05 -0700

    update projectuser model for email template: wip

--- shgroup/models.py
+++ shgroup/models.py
@@ -104,10 +104,11 @@ class ProjectUser(models.Model):
         # blocked sendinvite
         # if not self.pk:
         #     self.sendInvite = True
+        print(self)
 
         super(ProjectUser, self).save(*args, **kwargs)
 
-        if (not self.pk) & (self.sendInvite):
+        if self.sendInvite:
             project = Project.objects.get(id=self.survey.project.id)
             survey = Survey.objects.get(id=self.survey.id)
             user = User.objects.get(id=self.user.id)
