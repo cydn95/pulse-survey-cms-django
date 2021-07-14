7375a1040cb2bc4bb6901e417b5864e3347f13d5
cydn95 <devstar.sky20@gmail.com>
2021-07-14 02:58:56 -0700

    add email functionality for reset password

--- snippets/views.py
+++ snippets/views.py
@@ -6,6 +6,11 @@ import json
 from django.http import HttpResponse
 from django.middleware import csrf
 from django.core.mail import send_mail, EmailMultiAlternatives
+from django.dispatch import receiver
+from django.urls import reverse
+
+from django_rest_passwordreset.signals import reset_password_token_created
+
 from django.template.loader import get_template, render_to_string
 from django.conf import settings
 
@@ -167,6 +172,42 @@ stopwords += ['viz', 'vs', 'want', 'wants', 'way']
 stopwords += ['welcome', 'went', 'willing', 'wish', 'wonder']
 stopwords += ['yes', 'zero']
 
+@receiver(reset_password_token_created)
+def password_reset_token_created(sender, instance, reset_password_token, *args, **kwargs):
+    """
+    Handles password reset tokens
+    When a token is created, an e-mail needs to be sent to the user
+    :param sender: View Class that sent the signal
+    :param instance: View Instance that sent the signal
+    :param reset_password_token: Token Model Object
+    :param args:
+    :param kwargs:
+    :return:
+    """
+    # send an e-mail to the user
+    context = {
+        'current_user': reset_password_token.user,
+        # 'username': reset_password_token.username,
+        'email': reset_password_token.email,
+        'reset_password_url': "{}?token={}".format(instance.request.build_absolute_url(reverse('password_reset:reset-password-confirm')), reset_password_token.key)
+    }
+
+    # render email text
+    email_html_message = render_to_string('email/user_reset_password.html', context)
+    email_plaintext_message = render_to_string('email/user_reset_password.txt', context)
+
+    msg = EmailMultiAlternatives(
+        # title:
+        "Password Reset for {title}".format(title="Some website title"),
+        # message:
+        email_plaintext_message,
+        # from:
+        "pulse@projectai.com",
+        # to:
+        [reset_password_token.user.email]
+    )
+    msg.attach_alternative(email_html_message, "text/html")
+    msg.send()
 
 def preApiCheck(survey, projectUser):
     # prefix check
