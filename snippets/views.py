8a5522321bee42d1dbd0341ab528b95b8088bda6
cydn95 <devstar.sky20@gmail.com>
2021-07-14 03:04:02 -0700

    fix typo issue in reset password object

--- snippets/views.py
+++ snippets/views.py
@@ -187,8 +187,8 @@ def password_reset_token_created(sender, instance, reset_password_token, *args, 
     # send an e-mail to the user
     context = {
         'current_user': reset_password_token.user,
-        # 'username': reset_password_token.username,
-        'email': reset_password_token.email,
+        # 'username': reset_password_token.user.username,
+        'email': reset_password_token.user.email,
         'reset_password_url': "{}?token={}".format(instance.request.build_absolute_url(reverse('password_reset:reset-password-confirm')), reset_password_token.key)
     }
 
