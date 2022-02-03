5265ca712e430324456c5f89e4c3b430bb996e63
cydn95 <devstar.sky20@gmail.com>
2022-02-02 23:15:20 -0800

    fix subdriver api issue

--- snippets/views.py
+++ snippets/views.py
@@ -1903,6 +1903,60 @@ class SHCategoryViewSet(viewsets.ModelViewSet):
             queryset = queryset.filter(mapType__id=mapType)
         return queryset
 
+# class SubDriverViewSet(viewsets.ModelViewSet):
+#     permission_classes = [permissions.IsAuthenticated, permissions.IsAuthenticatedOrReadOnly]
+#     queryset = Driver.objects.all()
+#     serializer_class = DriverSubDriverSerializer
+
+#     def get_queryset(self):
+#         queryset = Driver.objects.all()
+#         survey = self.request.query_params.get('survey', None)
+#         driver = self.request.query_params.get('driver', None)
+
+#         if (survey is not None) & (driver is not None):
+#             queryset = queryset.filter(survey__id=survey, id=driver)
+#         elif survey is not None:
+#             queryset = queryset.filter(survey__id=survey)
+#         elif driver is not None:
+#             queryset = queryset.filter(id=driver)
+
+#         return queryset
+
+#     def list(self, request, *args, **kwargs):
+#         response = super().list(request, *args, **kwargs)
+#         survey = self.request.query_params.get('survey', None)
+#         driver = self.request.query_params.get('driver', None)
+
+#         for i in range(len(response.data)):
+#             amsubdriver_queryset = ''
+#             aosubdriver_queryset = ''
+#             if (survey is not None) & (driver is not None):
+#                 amsubdriver_queryset = AMQuestion.objects.filter(survey__id=survey, driver__id=driver).values('subdriver').distinct()
+#                 aosubdriver_queryset = AOQuestion.objects.filter(survey__id=survey, driver__id=driver).values('subdriver').distinct()
+#             elif survey is not None:
+#                 amsubdriver_queryset = AMQuestion.objects.filter(survey__id=survey).values('subdriver').distinct()
+#                 aosubdriver_queryset = AOQuestion.objects.filter(survey__id=survey).values('subdriver').distinct()
+#             elif driver is not None:
+#                 amsubdriver_queryset = AMQuestion.objects.filter(driver__id=driver).values('subdriver').distinct()
+#                 aosubdriver_queryset = AOQuestion.objects.filter(driver__id=driver).values('subdriver').distinct()
+#             else:
+#                 amsubdriver_queryset = AMQuestion.objects.all().values('subdriver').distinct()
+#                 aosubdriver_queryset = AOQuestion.objects.all().values('subdriver').distinct()
+
+#             amsubdriver_serializer = AMQuestionSubDriverSerializer(amsubdriver_queryset, many=True)
+#             aosubdriver_serializer = AOQuestionSubDriverSerializer(aosubdriver_queryset, many=True)
+
+#             response.data[i]['subdriver'] = []
+#             response.data[i]['subdriver'] = []
+
+#             for item in amsubdriver_serializer.data:
+#                 response.data[i]['subdriver'].append(item['subdriver'])
+#             for item in aosubdriver_serializer.data:
+#                 if not item['subdriver'] in response.data[i]['subdriver']:
+#                     response.data[i]['subdriver'].append(item['subdriver'])
+
+#         return response
+
 # subdriver api
 class SubDriverViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, permissions.IsAuthenticatedOrReadOnly]
