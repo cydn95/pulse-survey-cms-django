7f13c24621416bd2349e9c9d086ef0a305f22d55
cydn95 <devstar.sky20@gmail.com>
2022-04-01 14:33:59 +0800

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -431,8 +431,7 @@ CORS_ORIGIN_WHITELIST = [
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'Pulse by ProjectAI <pulse@projectai.com>'
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST='smtp.projectai.com'
+EMAIL_BACKEND = 'django_ses.SESBackend'
 
 AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
